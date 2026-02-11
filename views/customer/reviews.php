8642451dcec7db5f21ae5ef1c501ee17b213722c
Sanduni Basnayake <sandunikbasnayaka@gmail.com>
2026-02-11 10:27:51 +0530

    review page wh smart link fixed

--- views/customer/reviews.php
+++ views/customer/reviews.php
@@ -161,8 +161,9 @@ require_once 'views/layouts/customer_header.php';
                     }
                 ?>
                 <a href="<?= $waLink ?>" target="_blank">
-                    <img src="<?= BASE_URL ?>assets\icons\whatsapp.png" style="width: 45px;"> 
+                    <img src="<?= BASE_URL ?>assets/icons/whatsapp.png" style="width: 45px;"> 
                 </a>
+                
 
 
             </div>
